2de26f7a20c16f5f8b8c3c3792e7428cd62df3c1
Terry Wambui <shikuwambui@gmail.com>
2023-08-27 16:54:00 +0300

    puppet client config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Seting up my client config file
+# using puppet to make changes to the default ssh config file
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
