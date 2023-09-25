9e4790898905c77974a7ec062940e2d6847473b6
Terry Wambui <shikuwambui@gmail.com>
2023-09-25 15:58:02 +0300

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""exports information to CSV format"""
+"""Exports to CSV"""
 
-import requests
-import sys
-import csv
+from requests import get
+from sys import argv
 
-if __name__ == "__main__":
-    # Retrieve user ID from CLI
-    user_id = sys.argv[1]
 
-    # Define the URL for the REST API
-    url = "https://jsonplaceholder.typicode.com/"
+if __name__ == '__main__':
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(url)
+    username = response.json().get('username')
 
-    # send a GET request to retrieve user info
-    user = requests.get(url + "users/{}".format(user_id)).json()
-
-    # get the username of the user
-    username = user.get("username")
-
-    # send a GET request to retrive the TODO list
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-    # open a CSV file for writing
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
+    tasks = response.json()
+    with open('{}.csv'.format(user_id), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(user_id, username, task.get('completed'),
+                               task.get('title')))
