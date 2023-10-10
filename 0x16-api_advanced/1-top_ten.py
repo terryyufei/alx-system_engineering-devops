15ad947e16200c2588fa2cf2edde3dcff01f4819
Terry Wambui <shikuwambui@gmail.com>
2023-10-10 14:03:08 +0300

    Top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 
 def top_ten(subreddit):
     """Query Reddit and print titles of the first 10 hot posts"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
     # Set a custom User-Agent to avoid too many requests error
     headers = {'User-Agent': 'My user Agent 1.0'}
@@ -15,10 +15,13 @@ def top_ten(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     # Chek if the request was successful and not redirect
-    if response.status_code == 200:
+    if response.status_code == 200 and not response.is_redirect:
         # Parse the JSON response and extract post titles
-        children = response.json().get('data').get('children')
-        for i in range(10):
-            print(children[i].get('data').get('title'))
+        post_data = response.json().get('data', {}).get('children', [])
+
+        # Print the titles of the first 10 hot posts
+        for i, post in enumerate(post_data[:10], start=1):
+            post_title = post['data']['title']
+            print(f"{i}. {post_title}")
         else:
             print("None")
